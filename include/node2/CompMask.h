310dbded605b18200dd90e76adf67591d21f719e
zz <zhuguang@ejoy.com>
2018-02-28 11:41:46 +0800

    [ADDED] Traverse()

--- include/node2/CompMask.h
+++ include/node2/CompMask.h
@@ -3,6 +3,8 @@
 #include <node0/NodeComponent.h>
 #include <node0/typedef.h>
 
+#include <functional>
+
 namespace n2
 {
 
@@ -10,6 +12,8 @@ class CompMask : public n0::NodeComponent
 {
 public:
 	virtual const char* Type() const override { return TYPE_NAME; }
+
+	void Traverse(std::function<bool(const n0::SceneNodePtr&)> func) const;
 
 	void SetBaseNode(const n0::SceneNodePtr& base) { m_base = base; }
 	void SetMaskNode(const n0::SceneNodePtr& mask) { m_mask = mask; }
--- source/CompMask.cpp
+++ source/CompMask.cpp
@@ -5,4 +5,14 @@ namespace n2
 
 const char* const CompMask::TYPE_NAME = "n2_mask";
 
+void CompMask::Traverse(std::function<bool(const n0::SceneNodePtr&)> func) const
+{
+	if (m_base) {
+		func(m_base);
+	}
+	if (m_mask) {
+		func(m_mask);
+	}
+}
+
 }

472d2fabea80a691be85b6f534a07b84f51703da
zz <zhuguang@ejoy.com>
2018-03-04 08:36:39 +0800

    [ADDED] scale9 build

--- include/node2/CompScale9.h
+++ include/node2/CompScale9.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SM_Vector.h>
 #include <node0/NodeComponent.h>
 #include <node0/typedef.h>
 
@@ -10,22 +11,6 @@ namespace n2
 
 class CompScale9 : public n0::NodeComponent
 {
-public:
-	CompScale9();
-
-	virtual const char* Type() const override { return TYPE_NAME; }
-
-	void Traverse(std::function<bool(const n0::SceneNodePtr&)> func) const;
-
-	void SetNode(int idx, const n0::SceneNodePtr& node);
-
-	int GetWidth() const { return m_width; }
-	int GetHeight() const { return m_height; }
-	void SetWidth(int width) { m_width = width; }
-	void SetHeight(int height) { m_height = height; }
-
-	static const char* const TYPE_NAME;
-
 public:
 	enum Scale9Type
 	{
@@ -39,28 +24,64 @@ public:
 
 	enum Scale9Idx
 	{
-		S9_DOWN_LEFT	= 0,
-		S9_DOWN_CENTER	= 1,
-		S9_DOWN_RIGHT	= 2,
+		S9_DOWN_LEFT = 0,
+		S9_DOWN_CENTER = 1,
+		S9_DOWN_RIGHT = 2,
 
-		S9_MID_LEFT		= 3,
-		S9_MID_CENTER	= 4,
-		S9_MID_RIGHT	= 5,
+		S9_MID_LEFT = 3,
+		S9_MID_CENTER = 4,
+		S9_MID_RIGHT = 5,
 
-		S9_TOP_LEFT		= 6,
-		S9_TOP_CENTER	= 7,
-		S9_TOP_RIGHT	= 8,
+		S9_TOP_LEFT = 6,
+		S9_TOP_CENTER = 7,
+		S9_TOP_RIGHT = 8,
 	};
+
+public:
+	CompScale9();
+
+	virtual const char* Type() const override { return TYPE_NAME; }
+	virtual std::unique_ptr<n0::NodeComponent> Clone() const override;
+
+	void Traverse(std::function<bool(const n0::SceneNodePtr&)> func) const;
+
+	float GetWidth() const { return m_width; }
+	float GetHeight() const { return m_height; }
+	void  SetWidth(float width);
+	void  SetHeight(float height);
+
+	void Build(Scale9Type type, float w, float h, n0::SceneNodePtr grids[9],
+		int sz_left, int sz_right, int sz_top, int sz_down);
+
+	Scale9Type GetType() const { return m_type; }
+	static Scale9Type CheckType(const n0::SceneNodePtr grids[9]);
+
+	static const char* const TYPE_NAME;
+
+private:
+	void SetSize(float width, float height);
+
+	void ResizeNode(Scale9Idx idx, const sm::vec2& center,
+		float dst_w, float dst_h, bool no_scale_w, bool no_scale_h);
+
+	sm::vec2 GetChildSize(Scale9Idx idx) const;
+
+	float GetLeftWidth() const;
+	float GetRightWidth() const;
+	float GetDownHeight() const;
+	float GetTopHeight() const;
 
 private:
 	Scale9Type m_type;
+
+	float m_width, m_height;
 
 	// 6 7 8
 	// 3 4 5
 	// 0 1 2
 	n0::SceneNodePtr m_grids[9];
 
-	int m_width, m_height;
+	int m_sz_left, m_sz_right, m_sz_top, m_sz_down;
 
 }; // CompScale9
 
--- source/CompScale9.cpp
+++ source/CompScale9.cpp
@@ -1,4 +1,10 @@
 #include "node2/CompScale9.h"
+#include "node2/CompBoundingBox.h"
+
+#include <node0/SceneNode.h>
+#include <node2/CompTransform.h>
+
+#include <algorithm>
 
 namespace n2
 {
@@ -10,6 +16,30 @@ CompScale9::CompScale9()
 	, m_width(200)
 	, m_height(200)
 {
+	m_sz_left = m_sz_right = m_sz_top = m_sz_down = 0;
+}
+
+std::unique_ptr<n0::NodeComponent> CompScale9::Clone() const
+{
+	auto comp = std::make_unique<CompScale9>();
+
+	comp->m_type = m_type;
+
+	comp->m_width  = m_width;
+	comp->m_height = m_height;
+
+	for (int i = 0; i < 9; ++i) {
+		if (m_grids[i]) {
+			comp->m_grids[i] = m_grids[i]->Clone();
+		}
+	}
+
+	comp->m_sz_left  = m_sz_left;
+	comp->m_sz_right = m_sz_right;
+	comp->m_sz_top   = m_sz_top;
+	comp->m_sz_down  = m_sz_down;
+
+	return comp;
 }
 
 void CompScale9::Traverse(std::function<bool(const n0::SceneNodePtr&)> func) const
@@ -21,11 +51,349 @@ void CompScale9::Traverse(std::function<bool(const n0::SceneNodePtr&)> func) con
 	}
 }
 
-void CompScale9::SetNode(int idx, const n0::SceneNodePtr& node)
+void CompScale9::SetWidth(float width)
 {
-	if (idx >= 0 && idx < 9) {
-		m_grids[idx] = node;
+	if (m_width != width) {
+		SetSize(width, m_height);
 	}
+}
+
+void CompScale9::SetHeight(float height)
+{
+	if (m_height != height) {
+		SetSize(m_width, height);
+	}
+}
+
+void CompScale9::Build(Scale9Type type, float w, float h, n0::SceneNodePtr grids[9],
+	                   int sz_left, int sz_right, int sz_top, int sz_down)
+{
+	m_type = type;
+	m_width = m_height = 0;
+	for (int i = 0; i < 9; ++i) 
+	{
+		auto& src = grids[i];
+		if (src) {
+			m_grids[i] = src->Clone();
+		} else {
+			m_grids[i] = nullptr;
+		}
+	}
+	m_sz_left  = sz_left;
+	m_sz_right = sz_right;
+	m_sz_top   = sz_top;
+	m_sz_down  = sz_down;
+	SetSize(static_cast<float>(w), static_cast<float>(h));
+}
+
+CompScale9::Scale9Type CompScale9::CheckType(const n0::SceneNodePtr grids[9])
+{
+	Scale9Type type = S9_NULL;
+	do {
+		// S9_9GRID
+		type = S9_9GRID;
+		for (int i = 0; i < 9; ++i) {
+			if (!grids[i]) {
+				type = S9_NULL;
+				break;
+			}
+		}
+		if (type != S9_NULL) break;
+
+		// S9_9GRID_HOLLOW
+		type = S9_9GRID_HOLLOW;
+		for (int i = 0; i < 9; ++i) {
+			if (i == 4) {
+				continue;
+			}
+			if (!grids[i]) {
+				type = S9_NULL;
+				break;
+			}
+		}
+		if (type != S9_NULL) break;
+
+		// S9_6GRID_UPPER
+		type = S9_6GRID_UPPER;
+		for (int i = 3; i < 9; ++i) {
+			if (!grids[i]) {
+				type = S9_NULL;
+				break;
+			}
+		}
+		if (type != S9_NULL) break;
+
+		// S9_3GRID_HORI
+		if (grids[3] && grids[4] && grids[5]) {
+			type = S9_3GRID_HORI;			
+		}
+
+		// S9_3GRID_VERT
+		if (grids[1] && grids[4] && grids[7]) {
+			type = S9_3GRID_VERT;
+		}
+	} while (false);
+	return type;
+}
+
+void CompScale9::SetSize(float width, float height)
+{
+	if (m_width == width && m_height == height) {
+		return;
+	}
+
+	m_width  = width;
+	m_height = height;
+
+	switch (m_type)
+	{
+	case S9_9GRID:
+		{
+			float w0 = GetLeftWidth(),
+				  w2 = GetRightWidth(),
+				  w1 = width - w0 - w2;
+			float h0 = GetDownHeight(),
+				  h2 = GetTopHeight(),
+				  h1 = height - h0 - h2;
+
+			ResizeNode(S9_DOWN_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, -h0*0.5f-h1*0.5f), w0, h0, true, true);
+			ResizeNode(S9_DOWN_CENTER, sm::vec2(0.0f, -h0*0.5f-h1*0.5f), w1, h0, false, true);
+			ResizeNode(S9_DOWN_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, -h0*0.5f-h1*0.5f), w2, h0, true, true);
+
+			ResizeNode(S9_MID_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, 0.0f), w0, h1, true, false);
+			ResizeNode(S9_MID_CENTER, sm::vec2(0.0f, 0.0f), w1, h1, false, false);
+			ResizeNode(S9_MID_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, 0.0f), w2, h1, true, false);
+
+			ResizeNode(S9_TOP_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, h1*0.5f+h2*0.5f), w0, h2, true, true);
+			ResizeNode(S9_TOP_CENTER, sm::vec2(0.0f, h1*0.5f+h2*0.5f), w1, h2, false, true);
+			ResizeNode(S9_TOP_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, h1*0.5f+h2*0.5f), w2, h2, true, true);
+		}
+		break;
+	case S9_9GRID_HOLLOW:
+		{
+			float w0 = GetLeftWidth(),
+				  w2 = GetRightWidth(),
+				  w1 = width - w0 - w2;
+			float h0 = GetDownHeight(),
+				  h2 = GetTopHeight(),
+				  h1 = height - h0 - h2;
+
+			ResizeNode(S9_DOWN_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, -h0*0.5f-h1*0.5f), w0, h0, true, true);
+			ResizeNode(S9_DOWN_CENTER, sm::vec2(0.0f, -h0*0.5f-h1*0.5f), w1, h0, false, true);
+			ResizeNode(S9_DOWN_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, -h0*0.5f-h1*0.5f), w2, h0, true, true);
+
+			ResizeNode(S9_MID_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, 0.0f), w0, h1, true, false);
+			ResizeNode(S9_MID_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, 0.0f), w2, h1, true, false);
+
+			ResizeNode(S9_TOP_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, h1*0.5f+h2*0.5f), w0, h2, true, true);
+			ResizeNode(S9_TOP_CENTER, sm::vec2(0.0f, h1*0.5f+h2*0.5f), w1, h2, false, true);
+			ResizeNode(S9_TOP_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, h1*0.5f+h2*0.5f), w2, h2, true, true);
+		}
+		break;
+	case S9_6GRID_UPPER:
+		{
+			float w0 = GetLeftWidth(),
+				  w2 = GetRightWidth(),
+				  w1 = width - w0 - w2;
+			float h2 = GetTopHeight(),
+				  h1 = height - h2;
+
+			ResizeNode(S9_MID_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, 0.0f), w0, h1, true, false);
+			ResizeNode(S9_MID_CENTER, sm::vec2(0.0f, 0.0f), w1, h1, false, false);
+			ResizeNode(S9_MID_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, 0.0f), w2, h1, true, false);
+
+			ResizeNode(S9_TOP_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, h1*0.5f+h2*0.5f), w0, h2, true, true);
+			ResizeNode(S9_TOP_CENTER, sm::vec2(0.0f, h1*0.5f+h2*0.5f), w1, h2, false, true);
+			ResizeNode(S9_TOP_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, h1*0.5f+h2*0.5f), w2, h2, true, true);
+		}
+		break;
+	case S9_3GRID_HORI:
+		{
+			float w0 = GetLeftWidth(),
+				  w2 = GetRightWidth(),
+				  w1 = width - w0 - w2; 
+
+			ResizeNode(S9_MID_LEFT, sm::vec2(-w0*0.5f-w1*0.5f, 0.0f), w0, height, true, true);
+			ResizeNode(S9_MID_CENTER, sm::vec2(0.0f, 0.0f), w1, height, false, true);
+			ResizeNode(S9_MID_RIGHT, sm::vec2(w1*0.5f+w2*0.5f, 0.0f), w2, height, true, true);
+		}
+		break;
+	case S9_3GRID_VERT:
+		{
+			float h0 = GetDownHeight(),
+				  h2 = GetTopHeight(),
+				  h1 = height - h0 - h2;
+
+			ResizeNode(S9_DOWN_CENTER, sm::vec2(0.0f, -h0*0.5f-h1*0.5f), width, h0, true, true);
+			ResizeNode(S9_MID_CENTER, sm::vec2(0.0f, 0.0f), width, h1, true, false);
+			ResizeNode(S9_TOP_CENTER, sm::vec2(0.0f, h1*0.5f+h2*0.5f), width, h2, true, true);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void CompScale9::ResizeNode(Scale9Idx idx, const sm::vec2& center, float dst_w, 
+	                          float dst_h, bool no_scale_w, bool no_scale_h)
+{
+	if (idx < 0 || idx >= 9 || !m_grids[idx]) {
+		return;
+	}
+
+	auto& node = m_grids[idx];
+
+	if (dst_w < 0) { dst_w = 1; }
+	if (dst_h < 0) { dst_h = 1; }
+
+	auto& ctrans = node->GetComponent<n2::CompTransform>();
+	auto& trans = ctrans.GetTrans();
+	trans.SetPosition(center);
+
+	float src_w, src_h;
+	auto& cbb = node->GetComponent<n2::CompBoundingBox>();
+	auto& cbb_sz = cbb.GetSize();
+	src_w = cbb_sz.Width();
+	src_h = cbb_sz.Height();
+	if (src_w == 0 || src_h == 0) {
+		return;
+	}
+
+	const float times = trans.GetAngle() / SM_PI;
+	bool rotate = fabs(times - (int)times) > 0.1f;
+	if (m_sz_left != 0 && m_sz_right != 0 && m_sz_top != 0 && m_sz_down != 0)
+	{
+		if (rotate)
+		{
+			switch (idx)
+			{
+			case S9_DOWN_LEFT:
+				src_h = static_cast<float>(m_sz_left);
+				src_w = static_cast<float>(m_sz_down);
+				break;
+			case S9_DOWN_CENTER:
+				src_w = static_cast<float>(m_sz_down);
+				break;
+			case S9_DOWN_RIGHT:
+				src_h = static_cast<float>(m_sz_right);
+				src_w = static_cast<float>(m_sz_down);
+				break;
+			case S9_MID_LEFT:
+				src_h = static_cast<float>(m_sz_left);
+				break;
+			case S9_MID_RIGHT:
+				src_h = static_cast<float>(m_sz_right);
+				break;
+			case S9_TOP_LEFT:
+				src_h = static_cast<float>(m_sz_left);
+				src_w = static_cast<float>(m_sz_top);
+				break;
+			case S9_TOP_CENTER:
+				src_w = static_cast<float>(m_sz_top);
+				break;
+			case S9_TOP_RIGHT:
+				src_h = static_cast<float>(m_sz_right);
+				src_w = static_cast<float>(m_sz_top);
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			switch (idx)
+			{
+			case S9_DOWN_LEFT:
+				src_w = static_cast<float>(m_sz_left);
+				src_h = static_cast<float>(m_sz_down);
+				break;
+			case S9_DOWN_CENTER:
+				src_h = static_cast<float>(m_sz_down);
+				break;
+			case S9_DOWN_RIGHT:
+				src_w = static_cast<float>(m_sz_right);
+				src_h = static_cast<float>(m_sz_down);
+				break;
+			case S9_MID_LEFT:
+				src_w = static_cast<float>(m_sz_left);
+				break;
+			case S9_MID_RIGHT:
+				src_w = static_cast<float>(m_sz_right);
+				break;
+			case S9_TOP_LEFT:
+				src_w = static_cast<float>(m_sz_left);
+				src_h = static_cast<float>(m_sz_top);
+				break;
+			case S9_TOP_CENTER:
+				src_h = static_cast<float>(m_sz_top);
+				break;
+			case S9_TOP_RIGHT:
+				src_w = static_cast<float>(m_sz_right);
+				src_h = static_cast<float>(m_sz_top);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	const sm::vec2& old_scale = trans.GetScale();
+	sm::vec2 new_scale;	
+	if (rotate) {
+		new_scale.x = no_scale_h ? 1 : dst_h / src_w;
+		new_scale.y = no_scale_w ? 1 : dst_w / src_h;
+	} else {
+		new_scale.x = no_scale_w ? 1 : dst_w / src_w;
+		new_scale.y = no_scale_h ? 1 : dst_h / src_h;
+	}
+	if (old_scale.x < 0) {
+		new_scale.x = -new_scale.x;
+	}
+	if (old_scale.y < 0) {
+		new_scale.y = -new_scale.y;
+	}
+	trans.SetScale(new_scale);
+
+	auto& pos = trans.GetPosition();
+	auto& offset = trans.GetOffset();
+	auto  angle = trans.GetAngle();
+	trans.SetPosition(pos + sm::rotate_vector(offset, angle) - offset);
+}
+
+sm::vec2 CompScale9::GetChildSize(Scale9Idx idx) const
+{
+	if (idx < 0 || idx >= 9 || !m_grids[idx]) {
+		return sm::vec2(0, 0);
+	}
+
+	auto& node = m_grids[idx];
+	auto& cbb = node->GetComponent<n2::CompBoundingBox>();
+	auto& sz = cbb.GetSize();
+	return sm::vec2(sz.Width(), sz.Height());
+}
+
+float CompScale9::GetLeftWidth() const
+{
+	return m_sz_left == 0 ?
+		std::max(std::max(GetChildSize(S9_DOWN_LEFT).x, GetChildSize(S9_MID_LEFT).x), GetChildSize(S9_TOP_LEFT).x) : m_sz_left;
+}
+
+float CompScale9::GetRightWidth() const
+{
+	return m_sz_right == 0 ?
+		std::max(std::max(GetChildSize(S9_DOWN_RIGHT).x, GetChildSize(S9_MID_RIGHT).x), GetChildSize(S9_TOP_RIGHT).x) : m_sz_right;
+}
+
+float CompScale9::GetDownHeight() const
+{
+	return m_sz_down == 0 ?
+		std::max(std::max(GetChildSize(S9_DOWN_LEFT).y, GetChildSize(S9_DOWN_CENTER).y), GetChildSize(S9_DOWN_RIGHT).y) : m_sz_down;
+}
+
+float CompScale9::GetTopHeight() const
+{
+	return m_sz_top == 0 ?
+		std::max(std::max(GetChildSize(S9_TOP_LEFT).y, GetChildSize(S9_TOP_CENTER).y), GetChildSize(S9_TOP_RIGHT).y) : m_sz_top;
 }
 
 }

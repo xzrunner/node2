4073ced5a8108e3e8a9f93c1dc68c565fd40a27f
zz <zhuguang@ejoy.com>
2018-10-16 21:17:39 +0800

    [CHANGED] use rttr

--- source/CompShape.cpp
+++ source/CompShape.cpp
@@ -1,7 +1,6 @@
 #include "node2/CompShape.h"
 
 #include <guard/check.h>
-#include <geoshape/Factory.h>
 #include <geoshape/Shape.h>
 
 namespace n2
@@ -16,10 +15,7 @@ CompShape::CompShape(const std::shared_ptr<gs::Shape>& shape)
 
 std::unique_ptr<n0::NodeComp> CompShape::Clone(const n0::SceneNode& node) const
 {
-	assert(m_shape);
-	std::shared_ptr<gs::Shape> shape = gs::Factory::Instance()->Create(m_shape->GetClassInfo().GetClassName());
-	auto comp = std::make_unique<CompShape>(shape);
-	return comp;
+	return std::make_unique<CompShape>(m_shape->Clone());
 }
 
 }

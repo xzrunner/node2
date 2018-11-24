f999d8a61d8c12fc98f98e9cdf440aef61c3dd33
zz <zhuguang@ejoy.com>
2018-11-24 17:29:35 +0800

    [CHANGED] pt2::Mesh

--- include/node2/CompMesh.h
+++ include/node2/CompMesh.h
@@ -19,14 +19,14 @@ public:
 	virtual void Traverse(std::function<bool(const n0::SceneNodePtr&)> func,
 		bool inverse = false) const override {}
 
-	const std::unique_ptr<pt2::Mesh<n0::SceneNode>>& GetMesh() const { return m_mesh; }
-	std::unique_ptr<pt2::Mesh<n0::SceneNode>>& GetMesh() { return m_mesh; }
-	void SetMesh(std::unique_ptr<pt2::Mesh<n0::SceneNode>>& mesh) { m_mesh = std::move(mesh); }
+	const std::unique_ptr<pt2::Mesh<n0::SceneNodePtr>>& GetMesh() const { return m_mesh; }
+	std::unique_ptr<pt2::Mesh<n0::SceneNodePtr>>& GetMesh() { return m_mesh; }
+	void SetMesh(std::unique_ptr<pt2::Mesh<n0::SceneNodePtr>>& mesh) { m_mesh = std::move(mesh); }
 
 	static const char* const TYPE_NAME;
 
 private:
-	std::unique_ptr<pt2::Mesh<n0::SceneNode>> m_mesh = nullptr;
+	std::unique_ptr<pt2::Mesh<n0::SceneNodePtr>> m_mesh = nullptr;
 
 }; // CompMesh
 

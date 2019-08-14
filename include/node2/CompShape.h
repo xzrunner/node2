26f2b007eae64e01c19aaf4effda63b796a2657a
zz <zhuguang@ejoy.com>
2019-08-14 20:56:17 +0800

    rename

--- include/node2/CompShape.h
+++ include/node2/CompShape.h
@@ -3,7 +3,7 @@
 #include <node0/NodeComp.h>
 #include <node0/typedef.h>
 
-namespace gs { class Shape; }
+namespace gs { class Shape2D; }
 
 namespace n2
 {
@@ -11,7 +11,7 @@ namespace n2
 class CompShape : public n0::NodeComp
 {
 public:
-	CompShape(const std::shared_ptr<gs::Shape>& shape);
+	CompShape(const std::shared_ptr<gs::Shape2D>& shape);
 
 	virtual const char* Type() const override { return TYPE_NAME; }
 	virtual n0::CompID TypeID() const override {
@@ -19,13 +19,13 @@ public:
 	}
 	virtual std::unique_ptr<n0::NodeComp> Clone(const n0::SceneNode& node) const override;
 
-	void SetShape(const std::shared_ptr<gs::Shape>& shape) { m_shape = shape; }
+	void SetShape(const std::shared_ptr<gs::Shape2D>& shape) { m_shape = shape; }
 	auto& GetShape() const { return m_shape; }
 
 	static const char* const TYPE_NAME;
 
 private:
-	std::shared_ptr<gs::Shape> m_shape = nullptr;
+	std::shared_ptr<gs::Shape2D> m_shape = nullptr;
 
 }; // CompShape
 
--- source/CompShape.cpp
+++ source/CompShape.cpp
@@ -1,14 +1,14 @@
 #include "node2/CompShape.h"
 
 #include <guard/check.h>
-#include <geoshape/Shape.h>
+#include <geoshape/Shape2D.h>
 
 namespace n2
 {
 
 const char* const CompShape::TYPE_NAME = "n2_shape";
 
-CompShape::CompShape(const std::shared_ptr<gs::Shape>& shape)
+CompShape::CompShape(const std::shared_ptr<gs::Shape2D>& shape)
 	: m_shape(shape)
 {
 }
